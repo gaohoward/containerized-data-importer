c68437db2b36c8e31d81a16ce2960de6d047d945
Howard Gao <howard.gao@gmail.com>
2025-10-27 10:46:58 +0800

    fix

--- pkg/controller/clone/host-clone.go
+++ pkg/controller/clone/host-clone.go
@@ -213,6 +213,7 @@ func (p *HostClonePhase) createClaim(ctx context.Context) (*corev1.PersistentVol
 
 			if sourceVolumeMode := cc.GetVolumeMode(sourcePvc); sourceVolumeMode == corev1.PersistentVolumeFilesystem {
 				// If the source PVC is fs, just copy the size request
+				p.Log.Info("to copy,", "real", realSourcePvcSizeRequest, "original", claim.Spec.Resources.Requests[corev1.ResourceStorage])
 				claim.Spec.Resources.Requests[corev1.ResourceStorage] = realSourcePvcSizeRequest
 			} else {
 				// If the source PVC is block, we need to account for the overhead
